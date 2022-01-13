76ca6534a7251cf049bd3396c26d4c77f745a152
MinHanHsieh <92003830+MinHanHsieh@users.noreply.github.com>
2022-01-13 23:37:22 +0800

    Add files via upload

--- source/main.c
+++ source/main.c
@@ -14,29 +14,29 @@ void gotoxy(int x, int y);
 void changecolor();
 //初始化地圖 130*25
 char map[25][80];
-int score,game, Dx,Dy,Ox,Ox2,Ox3,jump_temp,jump,DS,T;
+int score, game, Dx, Dy, Ox, Ox2, Ox3, jump_temp, jump, DS, T;
 game = 0;
 jump = 0;
 score = 0;
 DS = 1;
 int main()
 {
-	system("color 70"); 
-	srand(time(0));
+	system("color 70");
+	srand(time(NULL));
 	int x, y;
 	Dx = 3; Ox = 48; Dy = 23; Ox2 = 65; Ox3 = 79; T = 1;
 	//初始化恐龍座標
-	map[Dy][Dx] = Dinosaur;           
+	map[Dy][Dx] = Dinosaur;
 	//初始化障礙座標
 	map[23][Ox] = Obstacle;
 	map[23][Ox2] = Obstacle;
 	map[23][Ox3] = Obstacle;
 	//地板
-	for ( x = 0; x < 25; x++)
+	for (x = 0; x < 25; x++)
 	{
-		for ( y = 0; y < 50; y++)
+		for (y = 0; y < 50; y++)
 		{
-			if (x==24)
+			if (x == 24)
 				map[x][y] = BAR;
 		}
 	}
@@ -48,32 +48,32 @@ int main()
 	{
 		HideCursor();
 		print_game();
-		map[23][Ox] =Air;
+		map[23][Ox] = Air;
 		map[23][Ox2] = Air;
 		map[23][Ox3] = Air;
 		Ox--; Ox2--; Ox3--;
 		map[23][Ox] = Obstacle;
 		map[23][Ox2] = Obstacle;
 		map[23][Ox3] = Obstacle;
-		if (map[Dy][Dx]==Obstacle)
+		if (map[Dy][Dx] == Obstacle)
 		{
 			break;
 		}
 		//判斷跳躍
-		if (kbhit()!=0)
+		if (kbhit() != 0)
 		{
 			jump_temp = getch();
-			if ((jump_temp==Space)&&(jump==0))
+			if ((jump_temp == Space) && (jump == 0))
 			{
 				jump = 1;
 			}
 			jump_temp == 0;
 		}
 		//跳躍移動
-		if (jump==1)
+		if (jump == 1)
 		{
 			map[Dy][Dx] = Air;
-			if((0<DS)&&(DS<5))
+			if ((0 < DS) && (DS < 5))
 			{
 				Dy--;
 				DS++;
@@ -82,7 +82,7 @@ int main()
 			{
 				DS = -1;
 			}
-			else if ((-5<DS)&&(DS < 0))
+			else if ((-5 < DS) && (DS < 0))
 			{
 				Dy++;
 				DS--;
@@ -104,15 +104,13 @@ int main()
 		{
 			map[23][0] = Air;
 			Ox2 = ((rand() % 30) + 50);
-			
+
 		}
 		else if (Ox3 == 0)
 		{
 			map[23][0] = Air;
 			Ox3 = ((rand() % 30) + 50);
 		}
-
-
 	}
 	printf("Game Over!!!!!!");
 }
@@ -120,7 +118,7 @@ void print_game(void) {
 	int i, j;
 	changecolor();
 	//根據地圖上每點的情況繪製遊戲（ i 表示 x 軸，j 表示 y 軸），按行列印
-	for (i = 0; i < 25;i++) 
+	for (i = 0; i < 25; i++)
 	{
 		for (j = 0; j < 50; j++) {
 			//空白地方
@@ -141,12 +139,11 @@ void print_game(void) {
 			}
 		}
 		putchar('\n');
-	} 
-	gotoxy(90, 25);
+	}
+	gotoxy(0, 0);
 	printf("score:%d", score);
 	Sleep(1);//休眠函式  
 	score++;
-	system("cls");  //清屏函式 配合下一次 print_game() 起到重新整理作用，包含在stdlib.h中
 }
 //隱藏光標
 void HideCursor(void)
@@ -163,6 +160,7 @@ void gotoxy(int x, int y)
 	coord.Y = y;
 	SetConsoleCursorPosition(handle, coord);//定位到handle這個視窗，把游標打在coord座標
 }
+
 void changecolor() {
 	int color;
 	if (score > 500)
@@ -190,4 +188,4 @@ void changecolor() {
 			break;
 		}
 	}
-}
+}      
