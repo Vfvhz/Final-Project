5ad04569a1f8e816fc1ac6aa8b3cd5ceb3ee105a
潘宥叡 <t110360210@ntut.org.tw>
2022-01-13 17:57:08 +0800

    拆.c檔

--- source/main.c
+++ source/main.c
@@ -11,10 +11,12 @@
 void print_game(void);
 void HideCursor(void);
 void gotoxy(int x, int y);
-void changecolor();
+void changecolor(int color);
 //初始化地圖 130*25
 char map[25][80];
 int score, game, Dx, Dy, Ox, Ox2, Ox3, jump_temp, jump, DS, T;
+clock_t begin, end;
+double cost;
 game = 0;
 jump = 0;
 score = 0;
@@ -43,6 +45,7 @@ int main()
 	//開始
 	printf("按任意鍵開始!");
 	getch();
+	begin = clock();
 	//主要內容
 	while (1)
 	{
@@ -112,11 +115,14 @@ int main()
 			Ox3 = ((rand() % 30) + 50);
 		}
 	}
-	printf("Game Over!!!!!!");
+	end = clock();
+	cost = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("\n遊玩時間: %0.2lf secs",cost);
+	printf("\nGame Over!!!!!!\n");
 }
 void print_game(void) {
 	int i, j;
-	changecolor();
+	changecolor(score);
 	//根據地圖上每點的情況繪製遊戲（ i 表示 x 軸，j 表示 y 軸），按行列印
 	for (i = 0; i < 25; i++)
 	{
@@ -145,13 +151,6 @@ void print_game(void) {
 	Sleep(1);//休眠函式  
 	score++;
 }
-//隱藏光標
-void HideCursor(void)
-{
-	CONSOLE_CURSOR_INFO cursor_info = { 1,0 };
-	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
-}
-
 void gotoxy(int x, int y)
 {
 	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -159,32 +158,4 @@ void gotoxy(int x, int y)
 	coord.X = x;
 	coord.Y = y;
 	SetConsoleCursorPosition(handle, coord);//定位到handle這個視窗，把游標打在coord座標
-}
-void changecolor() {
-	int color;
-	if (score > 500)
-	{
-		color = rand() % 6;
-		switch (color)
-		{
-		case 0:
-			system("color 42");
-			break;
-		case 1:
-			system("color 14");
-			break;
-		case 2:
-			system("color 41");
-			break;
-		case 3:
-			system("color 24");
-			break;
-		case 4:
-			system("color 56");
-			break;
-		case 5:
-			system("color 65");
-			break;
-		}
-	}
 }
